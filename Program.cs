87571922e0a8307071f40af77fc9449d7856e41a
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2023-07-25 18:27:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkQuestion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Letter;
+            Console.WriteLine("Please enter a letter");
+            Letter = Console.ReadLine();
+            switch (Letter)
+            {
+                case "A":
+                case "E":
+                case "I":
+                case "O":
+                case "U":
+                Console.WriteLine("This is a vowel");
+                    break;
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                Console.WriteLine("This is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("Invalid letter entered");
+                    break;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
